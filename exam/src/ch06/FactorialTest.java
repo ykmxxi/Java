e8b559a021b5cce49192e809ebd112ad0ab5f43d
ykmxxi <ykmxxi97@gmail.com>
2022-10-11 17:11:45 +0900

    재귀호출 예제 예외처리 추가

--- exam/src/ch06/FactorialTest.java
+++ exam/src/ch06/FactorialTest.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 
 public class FactorialTest {
 
+    // static 메서드는 인스턴스 생성 X
     static int factorial(int n) {
 
         int result = 0;
 
         if (n == 1) {
             result = 1;
+        } else if (n <= 0 || n > 12) {
+            System.out.println("유효하지 않은 값입니다. 프로그램 종료");
+            return -1;
         } else {
             result = n * factorial(n - 1);
         }
