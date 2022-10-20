4530c5d841199803796d194047f4e8b65971b89f
ykmxxi <ykmxxi97@gmail.com>
2022-10-20 20:44:20 +0900

    static import문 예제

---
+++ exam/src/ch07/staticimport/StaticImportTest.java
@@ -0,0 +1,18 @@
+package ch07.staticimport;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImportTest {
+
+    public static void main(String[] args) {
+
+        // System.out.println(Math.random());
+        out.println(random());
+
+        // System.out.println("Math.Pi = " + Math.PI);
+        out.println("Math.Pi = " + PI);
+    }
+
+
+}
