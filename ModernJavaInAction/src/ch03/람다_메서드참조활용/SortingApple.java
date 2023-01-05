88d560c6cad6780f556aa4c4774c0c768e9eaf2d
ykmxxi <ykmxxi97@gmail.com>
2023-01-05 17:27:56 +0900

    example(ModernJavaInAction): 3.8 람다 표현식 조합 추가

--- ModernJavaInAction/src/ch03/람다_메서드참조활용/SortingApple.java
+++ ModernJavaInAction/src/ch03/람다_메서드참조활용/SortingApple.java
@@ -11,13 +11,22 @@ public class SortingApple {
         List<Apple> inventory = Arrays.asList(
                 new Apple(160, "RED", "KR"),
                 new Apple(200, "GREEN", "KR"),
-                new Apple(80, "GREEN", "US"),
+                new Apple(200, "GREEN", "US"),
                 new Apple(130, "GREEN", "KR"),
                 new Apple(50, "RED", "US")
         );
 
         // Apple을 weight별로 비교해서 inventory를 sort하라.
-        inventory.sort(comparing(Apple::getWeight));
+        inventory.sort(comparing(Apple::getWeight)); // 오름차순
+
+        for (Apple apple : inventory) {
+            System.out.println(apple);
+        }
+
+        System.out.println();
+        inventory.sort(comparing(Apple::getWeight)
+                .reversed() // 내림차순
+                .thenComparing(Apple::getCountry)); // 두 사과의 무게가 같으면, 국가를 비교해 정렬
 
         for (Apple apple : inventory) {
             System.out.println(apple);
