5d74048886e6feeac4d29082e5db52a1cffb09b3
ykmxxi <ykmxxi97@gmail.com>
2023-01-07 14:56:03 +0900

    example(ModernJavaInAction): 4.3.2 외부 반복과 내부 반복 비교

--- ModernJavaInAction/src/ch04/StreamPipeline.java
+++ ModernJavaInAction/src/ch04/StreamPipeline.java
@@ -2,11 +2,29 @@ package ch04;
 
 import static ch04.Dish.menu;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class StreamPipeline {
     public static void main(String[] args) {
+        // 외부 반복
+        List<String> threeHighCaloricDishNamesByIterator = new ArrayList<>();
+        Iterator<Dish> iterator = menu.iterator();
+
+        while (iterator.hasNext()) {
+            Dish dish = iterator.next();
+            if (dish.getCalories() > 300) {
+                threeHighCaloricDishNamesByIterator.add(dish.getName());
+            }
+            if (threeHighCaloricDishNamesByIterator.size() == 3) {
+                break;
+            }
+        }
+        System.out.println(threeHighCaloricDishNamesByIterator);
+
+        // 내부 반복
         List<String> threeHighCaloricDishNames = menu.stream() // 메뉴의 스트림을 얻는다.
                 .filter(dish -> dish.getCalories() > 300) // 300 칼로리 초과로 필터링
                 .map(Dish::getName) // 요리명을 추출
