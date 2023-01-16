58dc20b0461a4b083a493199c85dd3ca9414aef1
ykmxxi <ykmxxi97@gmail.com>
2023-01-16 15:19:49 +0900

    example(ModernJavaInAction): 6.2.3 문자열 연결

---
+++ ModernJavaInAction/src/ch06/리듀싱과요약/StreamJoining.java
@@ -0,0 +1,22 @@
+package ch06.리듀싱과요약;
+
+import static ch06.Dish.menu;
+
+import ch06.Dish;
+import java.util.stream.Collectors;
+
+public class StreamJoining {
+    public static void main(String[] args) {
+        String menus = menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.joining());
+        System.out.println(menus);
+        // porkbeefchickenfrench friesriceseason fruitpizzaprawnssalmon
+
+        String menusWithDelimiter = menu.stream()
+                .map(Dish::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(menusWithDelimiter);
+        // pork, beef, chicken, french fries, rice, season fruit, pizza, prawns, salmon
+    }
+}
