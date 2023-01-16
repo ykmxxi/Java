7893d220011a75396f402fc8cff0c728517266f5
ykmxxi <ykmxxi97@gmail.com>
2023-01-16 15:01:27 +0900

    example(ModernJavaInAction): 6.2.2 요약 연산

---
+++ ModernJavaInAction/src/ch06/리듀싱과요약/StreamSummarization.java
@@ -0,0 +1,25 @@
+package ch06.리듀싱과요약;
+
+import static ch06.Dish.menu;
+
+import ch06.Dish;
+import java.util.IntSummaryStatistics;
+import java.util.stream.Collectors;
+
+public class StreamSummarization {
+    public static void main(String[] args) {
+
+        int totalCalories = menu.stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println("모든 메뉴의 칼로리 합: " + totalCalories + "Kcal");
+
+        double averageCalories = menu.stream()
+                .collect(Collectors.averagingDouble(Dish::getCalories));
+        System.out.println("메뉴의 칼로리 평균: " + averageCalories + "Kcal");
+
+        IntSummaryStatistics menuStatistics = menu.stream()
+                .collect(Collectors.summarizingInt(Dish::getCalories));
+
+        System.out.println(menuStatistics);
+    }
+}
