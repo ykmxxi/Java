60002842cf6571e73266b5ffd50d051f37d4ccf5
ykmxxi <ykmxxi97@gmail.com>
2023-01-17 17:39:51 +0900

    example(ModernJavaInAction): 6.4.2 범위 숫자의 소수와 비소수로 분할

---
+++ ModernJavaInAction/src/ch06/분할/PartitionPrimeNumber.java
@@ -0,0 +1,28 @@
+package ch06.분할;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class PartitionPrimeNumber {
+
+    public static boolean isPrime(int candidate) {
+        return IntStream.rangeClosed(2, (int) Math.sqrt(candidate))
+                .noneMatch(i -> candidate % i == 0);
+    }
+
+    public static Map<Boolean, List<Integer>> getPartitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n)
+                .boxed()
+                .collect(Collectors.partitioningBy(PartitionPrimeNumber::isPrime));
+    }
+
+    public static void main(String[] args) {
+        List<Integer> primeNumbers = getPartitionPrimes(100).get(true);
+        List<Integer> nonePrimeNumbers = getPartitionPrimes(100).get(false);
+
+        System.out.println(primeNumbers);
+        System.out.println(nonePrimeNumbers);
+    }
+}
