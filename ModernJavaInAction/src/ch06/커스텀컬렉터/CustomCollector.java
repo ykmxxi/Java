be6c627379ef88028a550e4237bc2d92ade711f9
ykmxxi <ykmxxi97@gmail.com>
2023-01-18 11:27:06 +0900

    chore(ModernJavaInAction): 6.6 커스텀 컬렉터 주석 추가

--- ModernJavaInAction/src/ch06/커스텀컬렉터/CustomCollector.java
+++ ModernJavaInAction/src/ch06/커스텀컬렉터/CustomCollector.java
@@ -18,6 +18,10 @@ import java.util.stream.Collector;
 
 public class CustomCollector implements Collector<Integer, Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> {
 
+    /**
+     * 누적자로 사용할 맵: key:[true, false]
+     * @return 누적자
+     */
     @Override
     public Supplier<Map<Boolean, List<Integer>>> supplier() {
         return () -> new HashMap<>() {{
@@ -26,23 +30,35 @@ public class CustomCollector implements Collector<Integer, Map<Boolean, List<Int
         }};
     }
 
+    /**
+     * 스트림의 요소 수집 방법 결정
+     * @return 소수 리스트, 비소수 리스트
+     */
     @Override
     public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
         return (Map<Boolean, List<Integer>> acc, Integer candidate) -> {
-            acc.get(isPrime(acc.get(true), candidate))
-                    .add(candidate);
+            acc.get(isPrime(acc.get(true), candidate)) // isPrime()의 결과에 따라 소수 리스트와 비소수 리스트를 생성
+                    .add(candidate); // candidate를 알맞은 리스트에 추가
         };
     }
 
+    /**
+     * 수집 과정에서 두 부분 누적자를 합칠 수 있는 메서드
+     * @return 부분 누적자 병합결과
+     */
     @Override
     public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
-        return (Map<Boolean, List<Integer>> map1, Map<Boolean, List<Integer>> map2) -> {
+        return (Map<Boolean, List<Integer>> map1, Map<Boolean, List<Integer>> map2) -> { // 두 번째 맵을 첫 번째 맵에 병합
             map1.get(true).addAll(map2.get(true));
             map1.get(false).addAll(map2.get(false));
             return map1;
         };
     }
 
+    /**
+     *
+     * @return 항등 함수
+     */
     @Override
     public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
         // return i -> i;
